026b711cf729da1a572cf62437ca1f68d2ee61d9
Denis Lupookov <36776703+denislupookov@users.noreply.github.com>
2026-03-09 17:53:32 +0700

    Removed extends of tsconfig from root app, made setup for eslint and  installed additional babel package (#14)

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,30 @@
-import { eslintConfig } from 'tether-dev-docs'
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
-  ...eslintConfig,
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
           varsIgnorePattern: '^React$',
-          ignoreRestSiblings: true
-        }
-      ]
-    }
-  }
-]
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+];
