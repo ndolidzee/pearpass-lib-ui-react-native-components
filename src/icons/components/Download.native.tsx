fd07f38c9de58b25e1e05813793d83b743f106a5
ndolidze <n.dolidze@noxtton.com>
2026-03-31 16:36:29 +0400

    Add download icon

---
+++ src/icons/components/Download.native.tsx
@@ -0,0 +1,13 @@
+// This file is auto-generated. Do not edit it.
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+import type { SvgProps } from "react-native-svg";
+const SvgDownload = (props: SvgProps) => (
+  <Svg width={24} height={24} fill="none" viewBox="0 0 20 20" {...props}>
+    <Path
+      fill="currentColor"
+      d="M15.833 7.916H12.5v-5h-5v5H4.167L10 13.749zM9.167 9.583v-5h1.666v5h.975L10 11.39 8.192 9.583zm-5 5.833h11.666v1.667H4.167z"
+    />
+  </Svg>
+);
+export default SvgDownload;
